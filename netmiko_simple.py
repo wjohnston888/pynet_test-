94f01e99ca73c9e0c4f4d1e66e154420cc7c734d
Wayne Johnston <wayne.johnston@axia.com>
2017-06-23 11:06:59 -0700

    day 4

--- netmiko_simple.py
+++ netmiko_simple.py
@@ -4,13 +4,13 @@ from netmiko import ConnectHandler
 
 if __name__ == "__main__":
 
-    password = getpass("Enter router password: ")
-    pynet_rtr1 = {
-        'device_type': 'cisco_ios',
-        'ip':   '184.105.247.70',
+    password = getpass("Enter password: ")
+    srx = {
+        'device_type': 'juniper_junos',
+        'ip':   '184.105.247.76',
         'username': 'pyclass',
         'password': password
     }
 
-    net_connect = ConnectHandler(**pynet_rtr1)
+    net_connect = ConnectHandler(**srx)
     print net_connect.find_prompt()
